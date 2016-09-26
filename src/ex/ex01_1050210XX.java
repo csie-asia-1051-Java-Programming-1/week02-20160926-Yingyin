a0c32bba3e061264110c239e27cb98bfb0ea690b
Yingyin <105791120@live.asia.edu.tw>
2016-09-26 14:49:07 +0800

    ex01  杨钰莹 105791120

--- src/ex/ex01_1050210XX.java
+++ src/ex/ex01_1050210XX.java
@@ -1,17 +1,42 @@
 package ex;
+
+import java.util.Scanner;
+
+
 /*
  * Topic: 電話公司的計費方式是 600分鐘以下每分鐘0.5元；
  * 	600~1200分鐘電話費以9折計算；1200分鐘以上電話費以79折計算，
  *  讓使用者輸入單月使用分時間(分鐘)，算出電話費(元)
  * Date: 2016/09/26
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex01_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 Scanner sca = new Scanner(System.in);
+	 System.out.println("請輸入分鐘");
+		int n;
+		n=sca.nextInt();
+		float p=(float) 0.5;
+		int d=0;
+		if(n<600)
+		{
+			d=(int) (n*p);
+		}
+		else
+		{
+			if(n<1200)
+			{
+				d=(int) (n*p*0.9);
+			}
+			else
+			{
+				d=(int) (n*p*0.79);
+			}
+			
+		}
+	System.out.print("請輸出話費"+d);
 	}
 
 }
