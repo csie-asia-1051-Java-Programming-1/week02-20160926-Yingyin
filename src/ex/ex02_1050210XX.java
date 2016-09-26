7a0f86b88aa7709899ccc537470d71bee6b32063
Yingyin <105791120@live.asia.edu.tw>
2016-09-26 14:54:29 +0800

    ex02 杨钰莹 105791120

--- src/ex/ex02_1050210XX.java
+++ src/ex/ex02_1050210XX.java
@@ -1,17 +1,26 @@
 package ex;
+
+import java.util.Scanner;
 /*
  * Topic: 有一圓形，直徑為200，且中心座標為(0,0)。
  * 	請寫一支程式可以輸入「點」的座標，並判斷「點」是否在圓形的範圍內。
  * 	如果「點」的位置剛好在邊界的話也算是在圓形範圍內(例：x=100，y=0)。
  * Date: 2016/09/26
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex02_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sca = new Scanner(System.in);
+		double x=sca.nextDouble();
+		double y=sca.nextDouble();
+		if(x*x+y*y<=200*200)
+			System.out.println("在園內");
+		else
+		{
+			System.out.println("不是");
+		}
 	}
 
 }
