f16eaab66ae4c97c29f0012899f49298da9b2e4e
Yingyin <105791120@live.asia.edu.tw>
2016-10-02 14:03:05 +0800

    hw03 105791120 杨钰莹

--- src/hw/hw03_1050210XX.java
+++ src/hw/hw03_1050210XX.java
@@ -1,10 +1,20 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw03_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca = new Scanner(System.in);
+     int n=sca.nextInt();
+     if((n%400==0)|| (n%4==0&&n%100!=0))
+     {
+    	 System.out.println("閏年");
+     }
+     else 
+     {
+    	 System.out.println("不是閏年");
+     }
 	}
 
 }
